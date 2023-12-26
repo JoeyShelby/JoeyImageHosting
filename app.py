aacc0778e4cc79ed768be987d8c2c18862c41c84
JoeyShelby <2276852293@qq.com>
2023-12-26 23:12:09 +0800

    modified app.py struct

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify, send_from_directory
+from flask import Flask, render_template, jsonify, send_from_directory
 
 from config import UPLOAD_FOLDER
 from show import list_files
@@ -7,11 +7,9 @@ from upload import upload_images
 app = Flask(__name__, static_folder='static')
 
 
-# 返回文件夹里的文件内容，list
 @app.route('/')
-def hello_world():  # put application's code here
-    files_info = list_files()
-    return render_template('hello.html', files_info=files_info)
+def hello_world():
+    return render_template('index.html')
 
 
 # 图片路径转化,通过如 /abc.jpg，直接访问到 UPLOAD_FOLDER 下的 filename
@@ -20,12 +18,13 @@ def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 
-# 上传文件的接口
+# 上传文件接口
 @app.route('/upload_images', methods=['POST'])
 def handle_upload_images():
     return upload_images()
 
 
+# 返回所有图片 JSON 数据
 @app.route('/list_files', methods=['GET'])
 def handle_list_files():
     # 调用列出文件的处理函数
@@ -39,7 +38,7 @@ def handle_list_files():
 
 @app.errorhandler(404)
 def handler404(error):
-    return render_template('hello.html'), 404
+    return render_template('index.html'), 404
 
 
 if __name__ == '__main__':
